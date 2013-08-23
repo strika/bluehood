8763d7b4effe52d5faa23f2b2aa55eefce8f7e44
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2013-08-23 12:35:51 +0200

    Add sign up scenario and fix looks

--- test/bluehood/features/authentication.clj
+++ test/bluehood/features/authentication.clj
@@ -1,9 +1,16 @@
 (ns bluehood.features.authentication
   (:use kerodon.core
         kerodon.test
-        clojure.test
-        bluehood.features.schema)
-  (:require [bluehood.handler :refer [app]]))
+        clojure.test)
+  (:require [bluehood.handler :refer [app]]
+            [bluehood.models.schema :refer [db-spec]]))
+
+(use-fixtures :each
+              (fn [f]
+                (clojure.java.jdbc/with-connection db-spec
+                  (clojure.java.jdbc/transaction
+                    (clojure.java.jdbc/set-rollback-only)
+                    (f)))))
 
 (deftest user-can-see-sign-in-form-on-the-homepage
   (-> (session app)
@@ -12,3 +19,16 @@
         (has (text? "Welcome to bluehood")))
       (has (value? [:#id] ""))
       (has (value? [:#pass] ""))))
+
+(deftest user-sign-up
+  (let [user {:username "mike" :password "topsecret"}]
+    (-> (session app)
+      (visit "/")
+      (follow "Sign Up")
+      (fill-in "Username" (:username user))
+      (fill-in "Password" (:password user))
+      (fill-in "Password Confirmation" (:password user))
+      (press "Sign Up")
+      (follow-redirect)
+      (within [:#welcome]
+        (has (text? (str "Signed in as " (:username user))))))))
